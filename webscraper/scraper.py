6011b945b3b7ffceedc656ea4c64b094ac6e3cab
Jerin Andrews <jerinandrews125@gmail.com>
2025-07-19 11:31:49 -0500

    parallelization with thread pools

--- webscraper/scraper.py
+++ webscraper/scraper.py
@@ -1,3 +1,4 @@
+import bs4
 from bs4 import BeautifulSoup
 import requests
 import re
@@ -5,12 +6,14 @@ import threading
 from concurrent.futures import ThreadPoolExecutor, as_completed
 import time
 
+# TODO: You can turn this into a design pattern style class so that the scraper is abstracted and can be used for different websites like the NovelUpdates
+
 BASE_URL = ["https://www.mangaupdates.com/series?perpage=100&page=", "1"]
 #https://www.mangaupdates.com/series?perpage=25 -> perpage=100 (consider this)
 #https://www.mangaupdates.com/series?page=400
 
-def get_page_list(url):
-    print(f"Scraping {url}")
+def get_page_list(url: str) -> list[list[str]]: #  TODO: The return type is wrong
+    print(f"[{time.strftime('%H:%M:%S')}] Scraping '{url}'")
     page = requests.get("".join(url))
     soup = BeautifulSoup(page.text, 'html.parser')
     series_parent_div = soup.find_all("div", {"class": "col-12 col-lg-6 p-3 text"})
@@ -25,28 +28,25 @@ def get_page_list(url):
         
     return curr_page_series
 
-def scrape_iterative(page_count, link_list): # 182.40774083137512 sec
+def scrape_iterative(page_count: int, link_list: list[bs4.element.ResultSet]) -> None: # 182.40774083137512 sec
     for i in range(1, page_count + 1):
         curr_url = BASE_URL[0] + str(i)
         links = get_page_list(curr_url)
         link_list.extend(links)
 
-def scrape_batch(start, end, link_list_lock, link_list): # 53.673600912094116 sec
+def scrape_batch(start: int, end: int, link_list_lock: object, link_list: list[bs4.element.ResultSet]) -> None: # 53.673600912094116 sec
     for i in range(start, end+1):
         curr_url = BASE_URL[0] + str(i)
         links = get_page_list(curr_url)
         with link_list_lock:
             link_list.extend(links)
             
-def scrape_pool(page_num): # 33.65859413146973 sec
+def scrape_pool(page_num: int) -> list[list[str]]: # 27.73180317878723 sec |  TODO: The return type is wrong
     url = BASE_URL[0] + str(page_num)
-    print(f"Scraping {url}")
+    print(f"[{time.strftime('%H:%M:%S')}] Scraping '{url}'\t| Thread: {threading.current_thread().name}", flush=True)
     page = requests.get(url)
     soup = BeautifulSoup(page.text, 'html.parser')
     series_parent_div = soup.find_all("div", {"class": "col-12 col-lg-6 p-3 text"})
-
-    # <a href="https://www.mangaupdates.com/series/dkd0yz5" title="Click for Series Info">
-    # print(len(series_parent_div))
     
     curr_page_series = []
     for p in series_parent_div:
@@ -85,12 +85,16 @@ def scrape():
     # scrape_iterative(page_count, link_list)
     
     # Multithread pool processing
-    with ThreadPoolExecutor(max_workers=10) as executor:
+    with ThreadPoolExecutor(max_workers=10) as executor: # TODO: You can probably run some kind of testing to see which number of workers is optimal
         futures = [executor.submit(scrape_pool, i) for i in range(1, page_count + 1)]
         for future in as_completed(futures):
             link_list.extend(future.result())
     
     print(f"Total links collected: {len(link_list)}")
+    
+    with open("scraper_dump.txt", "w") as file:
+        for series_link in link_list:
+            file.write(str(series_link) + "\n")
     
 if __name__ == '__main__':
     start = time.time()
